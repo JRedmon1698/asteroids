fd2a0b25dfbeb10cf4a9c45d7458411db3cbe6ab
Joe Redmon <joesredmon@gmail.com>
2024-10-23 16:55:21 -0700

    adds asteroid and asteroidfield modules, adds new asteroids group and adds it to asteroidfield container

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -10,8 +12,13 @@ def main():
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    Player.containers = (updatable, drawable)
+    asteroids = pygame.sprite.Group()
 
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     dt = 0
