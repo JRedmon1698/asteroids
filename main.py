7a5b6657879f7388de2e38a291840230fd6b572b
Joe Redmon <joe.redmon@emsibg.com>
2024-10-25 08:42:58 -0700

    when an asteroid is hit with a bullet, split it into smaller, faster asteroids

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def main():
             for shot in shots:
                 if shot.checkForCollision(asteroid):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         screen.fill('black')
 
